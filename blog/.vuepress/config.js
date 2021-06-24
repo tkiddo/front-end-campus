0d4e7fa47841685e22d21ad3d47d81f7bb7e9cca
tangkaiqiang <justforgit@163.com>
2021-06-24 19:03:37 +0800

    blog config

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -1,10 +1,24 @@
 module.exports = {
-  title: '唐凯强的博客', // Title for the site. This will be displayed in the navbar.
+  title: '唐凯强的网络博客', // Title for the site. This will be displayed in the navbar.
   theme: '@vuepress/theme-blog',
   themeConfig: {
     // Please keep looking down to see the available options.
     repo: 'tkiddo/front-end-campus',
-    repoLabel: 'Github'
+    repoLabel: 'Github',
+    nav: [
+      {
+        text: '博客',
+        link: '/'
+      },
+      {
+        text: '标签',
+        link: '/tag/'
+      },
+      {
+        text: 'Github',
+        link: 'https://github.com/tkiddo/front-end-campus'
+      }
+    ]
   },
   base: '/front-end-campus/'
 };
