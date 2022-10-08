ab8c9aff6187c2326f3d5e59ebe40b6f5bbd3950
tkiddo <justforgit@163.com>
2022-10-08 14:39:40 +0800

    feat: update nav

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -1,28 +1,32 @@
 module.exports = {
-  title: '唐凯强的网络博客', // Title for the site. This will be displayed in the navbar.
-  theme: '@vuepress/theme-blog',
+  title: "唐凯强的网络博客", // Title for the site. This will be displayed in the navbar.
+  theme: "@vuepress/theme-blog",
   themeConfig: {
     // Please keep looking down to see the available options.
-    repo: 'tkiddo/front-end-campus',
-    repoLabel: 'Github',
+    repo: "tkiddo/front-end-campus",
+    repoLabel: "Github",
     nav: [
       {
-        text: '博客',
-        link: '/'
+        text: "博客",
+        link: "/",
       },
       {
-        text: '标签',
-        link: '/tag/'
+        text: "标签",
+        link: "/tag/",
       },
       {
-        text: '收藏',
-        link: '/2020/10/19/projects/'
+        text: "收藏",
+        link: "/2020/10/19/projects/",
       },
       {
-        text: 'Github',
-        link: 'https://github.com/tkiddo/front-end-campus'
-      }
-    ]
+        text: "导航",
+        link: "/2022/10/08/nav/",
+      },
+      {
+        text: "Github",
+        link: "https://github.com/tkiddo/front-end-campus",
+      },
+    ],
   },
-  base: '/front-end-campus/'
+  base: "/front-end-campus/",
 };
